b8cb1e53092388113f15f1a2e836b7a837468896
Eric Ericsson <eokpoti19@gmai.com>
2023-06-07 10:26:17 +0000

    fonts family added

--- pages/_document.js
+++ pages/_document.js
@@ -11,9 +11,8 @@ class MyDocument extends Document {
       <Html>
         <Head>
         {/* <script src="https://unpkg.com/flowbite@1.4.1/dist/flowbite.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
-
-        <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;500;600;700;900&family=Open+Sans:wght@300;400;500;600&family=Playfair+Display:wght@500;600;900&display=swap" rel="stylesheet" /> */}
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script> */}
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Lobster+Two:ital@1&family=Nobile:wght@400;500;700&family=Playball&family=Playfair+Display:ital,wght@0,400;0,500;0,700;1,400;1,500&display=swap" rel="stylesheet" />
         </Head>
         <body className="overflow-x-hidden">
           <Main />
