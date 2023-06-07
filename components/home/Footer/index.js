f06978f2fb6578223dd3faefd8321f1e82f7f3db
Eric Ericsson <eokpoti19@gmai.com>
2023-06-07 10:09:47 +0000

    updates

--- components/home/Footer/index.js
+++ components/home/Footer/index.js
@@ -1,29 +1,28 @@
-
 const Footer = () => {
   return (
     <>
       <div className="divider"></div>
-    <div className="w-full flex items-center -mt-8 px-4 md:px-10 h-16">
-      <div className="flex items-center space-x-1">
-            <svg
-              className="w-10 h-10"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 84.85 109.92"
-            >
-              <g data-name="Layer 2">
-                <path
-                  d="M80 9.75C76.9 7.68 58-4.41 34.39 1.72A56.42 56.42 0 0 0 5.46 20.36c-.82 1.25-7 10.92-3.21 22.18a25.11 25.11 0 0 0 9.64 12.54Q5.95 77.41 0 99.75a64.82 64.82 0 0 0 39.54 10 63.67 63.67 0 0 0 29.25-10C80.17 92.28 86.34 79.46 84.54 67c-1.9-13.13-11.82-20.44-13.83-21.86Z"
-                  style={{
-                    fill: "#243b76",
-                  }}
-                />
-              </g>
-            </svg>
-            <span className="">Student Space, 2023</span>
-          </div>
-    </div>
+      <div className="w-full flex items-center -mt-8 px-4 md:px-10 h-16">
+        <div className="flex items-center space-x-1">
+          <svg
+            className="w-10 h-10"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 84.85 109.92"
+          >
+            <g data-name="Layer 2">
+              <path
+                d="M80 9.75C76.9 7.68 58-4.41 34.39 1.72A56.42 56.42 0 0 0 5.46 20.36c-.82 1.25-7 10.92-3.21 22.18a25.11 25.11 0 0 0 9.64 12.54Q5.95 77.41 0 99.75a64.82 64.82 0 0 0 39.54 10 63.67 63.67 0 0 0 29.25-10C80.17 92.28 86.34 79.46 84.54 67c-1.9-13.13-11.82-20.44-13.83-21.86Z"
+                style={{
+                  fill: "#243b76",
+                }}
+              />
+            </g>
+          </svg>
+          <span className="">Student Space, 2023</span>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- components/home/OtherWorks/index.js
+++ components/home/OtherWorks/index.js
@@ -1,10 +1,55 @@
-
 const OtherWorks = () => {
   return (
-    <div>
-      OtherWorks
+    <div className="mt-20 mx-4 md:mx-10">
+      <div className="text-4xl mb-6 font-semibold text-secondary_dark">
+        Guides to help you grow
+      </div>
+      <div className="grid grid-cols-none sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div>
+          <div className="w-[100%] h-60 md:h-52 lg:h-60 bg-black rounded-md text-light"></div>
+          <div className="mt-3">
+            <span className="font-semibold text-lg text-secondary_dark">
+              How to start an online business from home
+            </span>{" "}
+            <br />
+            <span className="opacity-70">
+              A step-by-step guide to learn everything you need to know about
+              successfully starting an online business from home to find the
+              flexibility, and freedom you desire.
+            </span>
+          </div>
+        </div>
+        <div>
+          <div className="w-[100%] h-60 md:h-52 lg:h-60 bg-black rounded-md text-light"></div>
+          <div className="mt-3">
+            <span className="font-semibold text-lg text-secondary_dark">
+              How to start an online business from home
+            </span>{" "}
+            <br />
+            <span className="opacity-70">
+              A step-by-step guide to learn everything you need to know about
+              successfully starting an online business from home to find the
+              flexibility, and freedom you desire.
+            </span>
+          </div>
+        </div>
+        <div>
+          <div className="w-[100%] h-60 md:h-52 lg:h-60 bg-black rounded-md text-light"></div>
+          <div className="mt-3">
+            <span className="font-semibold text-lg text-secondary_dark">
+              How to start an online business from home
+            </span>{" "}
+            <br />
+            <span className="opacity-70">
+              A step-by-step guide to learn everything you need to know about
+              successfully starting an online business from home to find the
+              flexibility, and freedom you desire.
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default OtherWorks
+export default OtherWorks;
